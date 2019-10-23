c9e93b4d0c35f9c255e25617cd958b39072b25a9
GianlucaMeola <40436065+GianlucaMeola@users.noreply.github.com>
2019-10-23 22:46:20 +0200

    db changes

--- src/app/customers/customer.ts
+++ src/app/customers/customer.ts
@@ -1,11 +1,7 @@
 export interface Icustomer {
     customerId: number;
     customerName: string;
-    customerCode: string;
-    releaseDate: string;
-    description: string;
-    price: number;
-    starRating: number;
-    imageUrl: string;
+    customerEmail: string;
+    releasePhoneNumber: string;
 }
 
